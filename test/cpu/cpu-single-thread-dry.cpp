508bcc123e522c718a7a6a2181da97182f381cfe
Yichao Yu <yyc1992@gmail.com>
2020-12-21 10:40:33 -0500

    [test/cpu] license date

--- test/cpu/cpu-single-thread-dry.cpp
+++ test/cpu/cpu-single-thread-dry.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
- *   Copyright (c) 2019 - 2019 Yichao Yu <yyc1992@gmail.com>             *
+ *   Copyright (c) 2019 - 2020 Yichao Yu <yyc1992@gmail.com>             *
  *                                                                       *
  *   This library is free software; you can redistribute it and/or       *
  *   modify it under the terms of the GNU Lesser General Public          *
