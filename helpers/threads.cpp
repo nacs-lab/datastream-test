ecc6162b072f81f0d260f62c95364a3cbf2f65a8
Yichao Yu <yyc1992@gmail.com>
2020-12-22 15:03:26 -0500

    [helpers] thread start and pinning

---
+++ helpers/threads.cpp
@@ -0,0 +1,149 @@
+/*************************************************************************
+ *   Copyright (c) 2020 - 2020 Yichao Yu <yyc1992@gmail.com>             *
+ *                                                                       *
+ *   This library is free software; you can redistribute it and/or       *
+ *   modify it under the terms of the GNU Lesser General Public          *
+ *   License as published by the Free Software Foundation; either        *
+ *   version 3.0 of the License, or (at your option) any later version.  *
+ *                                                                       *
+ *   This library is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU    *
+ *   Lesser General Public License for more details.                     *
+ *                                                                       *
+ *   You should have received a copy of the GNU Lesser General Public    *
+ *   License along with this library. If not,                            *
+ *   see <http://www.gnu.org/licenses/>.                                 *
+ *************************************************************************/
+
+#include "threads.h"
+
+#include <pthread.h>
+#include <sched.h>
+#include <stdlib.h>
+
+#include <memory>
+#include <stdexcept>
+#include <system_error>
+
+namespace Thread {
+
+using namespace NaCs;
+
+NACS_EXPORT() void pin(int cpu)
+{
+    auto self = pthread_self();
+
+    int ret;
+    if (likely(cpu < CPU_SETSIZE)) {
+        cpu_set_t cpuset;
+        CPU_ZERO(&cpuset);
+        CPU_SET(cpu, &cpuset);
+        ret = pthread_setaffinity_np(self, sizeof(cpu_set_t), &cpuset);
+    }
+    else {
+        auto cpuset = CPU_ALLOC(cpu + 1);
+        auto setsize = CPU_ALLOC_SIZE(cpu + 1);
+        CPU_ZERO_S(setsize, cpuset);
+        CPU_SET_S(cpu, setsize, cpuset);
+        ret = pthread_setaffinity_np(self, setsize, cpuset);
+        CPU_FREE(cpuset);
+    }
+    if (ret != 0) {
+        throw std::system_error(-ret, std::system_category(),
+                                "Failed to set thread affinity.");
+    }
+}
+
+NACS_EXPORT() std::vector<int> parse_cpulist(const char *cpulist)
+{
+    if (!cpulist || !*cpulist)
+        return {};
+
+    std::vector<int> cpus;
+
+    for (const char *p = cpulist; *p;) {
+        if (*p < '0' || *p > '9')
+            throw std::runtime_error(std::string("Invalid cpulist: ") + cpulist);
+        char *p2;
+        int cpu = (int)strtol(p, &p2, 10);
+        if (p2 == p)
+            throw std::runtime_error(std::string("Invalid cpulist: ") + cpulist);
+        if (*p2 == 0) {
+            cpus.push_back(cpu);
+            break;
+        }
+        else if (*p2 == ',') {
+            cpus.push_back(cpu);
+            p = p2 + 1;
+            continue;
+        }
+        else if (*p2 != '-') {
+            throw std::runtime_error(std::string("Invalid cpulist: ") + cpulist);
+        }
+
+        p = p2 + 1;
+        int cpu2 = (int)strtol(p, &p2, 10);
+        if (p2 == p)
+            throw std::runtime_error(std::string("Invalid cpulist: ") + cpulist);
+        if (*p2 == 0) {
+            for (int c = cpu; c <= cpu2; c++)
+                cpus.push_back(c);
+            break;
+        }
+        else if (*p2 == ',') {
+            for (int c = cpu; c <= cpu2; c++)
+                cpus.push_back(c);
+            p = p2 + 1;
+            continue;
+        }
+        throw std::runtime_error(std::string("Invalid cpulist: ") + cpulist);
+    }
+
+    return cpus;
+}
+
+namespace {
+
+struct thread_data {
+    std::function<void(int)> cb;
+    int id;
+    int cpu;
+    static void *thread_fun(void *_data)
+    {
+        std::unique_ptr<thread_data> data{(thread_data*)_data};
+        if (data->cpu >= 0)
+            pin(data->cpu);
+        data->cb(data->id);
+        return nullptr;
+    }
+    static void start(std::function<void(int)> cb, int id, int cpu=-1)
+    {
+        auto data = new thread_data{cb, id, cpu};
+        pthread_t thread;
+        int ret = pthread_create(&thread, nullptr, thread_fun, data);
+        if (ret != 0) {
+            delete data;
+            throw std::system_error(-ret, std::system_category(),
+                                    "Failed to start thread.");
+        }
+    }
+};
+
+}
+
+NACS_EXPORT() void start(int nthreads, std::function<void(int)> cb)
+{
+    for (int i = 0; i < nthreads; i++) {
+        thread_data::start(cb, i);
+    }
+}
+
+NACS_EXPORT() void start(const std::vector<int> &cpus, std::function<void(int)> cb)
+{
+    for (int i = 0; i < (int)cpus.size(); i++) {
+        thread_data::start(cb, i, cpus[i]);
+    }
+}
+
+}
---
+++ helpers/threads.h
@@ -0,0 +1,38 @@
+/*************************************************************************
+ *   Copyright (c) 2020 - 2020 Yichao Yu <yyc1992@gmail.com>             *
+ *                                                                       *
+ *   This library is free software; you can redistribute it and/or       *
+ *   modify it under the terms of the GNU Lesser General Public          *
+ *   License as published by the Free Software Foundation; either        *
+ *   version 3.0 of the License, or (at your option) any later version.  *
+ *                                                                       *
+ *   This library is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU    *
+ *   Lesser General Public License for more details.                     *
+ *                                                                       *
+ *   You should have received a copy of the GNU Lesser General Public    *
+ *   License along with this library. If not,                            *
+ *   see <http://www.gnu.org/licenses/>.                                 *
+ *************************************************************************/
+
+#ifndef HELPERS_THREAD_H
+#define HELPERS_THREAD_H
+
+#include <nacs-utils/utils.h>
+
+#include <functional>
+#include <vector>
+
+namespace Thread {
+
+NACS_EXPORT(ds_helper) void pin(int cpu);
+
+NACS_EXPORT(ds_helper) std::vector<int> parse_cpulist(const char *cpulist);
+
+NACS_EXPORT(ds_helper) void start(int nthreads, std::function<void(int)> cb);
+NACS_EXPORT(ds_helper) void start(const std::vector<int> &cpus, std::function<void(int)> cb);
+
+}
+
+#endif
