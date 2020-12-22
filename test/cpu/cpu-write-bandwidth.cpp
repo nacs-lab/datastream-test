3886b7b71f7e3c682a5a7847f62fc509e68167c6
Yichao Yu <yyc1992@gmail.com>
2020-12-22 10:28:01 -0500

    [test/cpu] More repitition

--- test/cpu/cpu-write-bandwidth.cpp
+++ test/cpu/cpu-write-bandwidth.cpp
@@ -110,50 +110,50 @@ static void runtests()
     if (host.test_feature(X86::Feature::avx512f) &&
         host.test_feature(X86::Feature::avx512dq)) {
         std::cout << "AVX512:" << std::endl;
-        time_run<avx512::Kernel>(32 * 2 * 1024 * 1024, 1024, buff.get());
-        time_run<avx512::Kernel>(32 * 16 * 1024, 128 * 1024, buff.get());
-        time_run<avx512::Kernel>(32 * 2 * 1024, 1024 * 1024, buff.get());
-        time_run<avx512::Kernel>(32 * 128, 16 * 1024 * 1024, buff.get());
+        time_run<avx512::Kernel>(32 * 4 * 1024 * 1024, 1024, buff.get());
+        time_run<avx512::Kernel>(32 * 32 * 1024, 128 * 1024, buff.get());
+        time_run<avx512::Kernel>(32 * 4 * 1024, 1024 * 1024, buff.get());
+        time_run<avx512::Kernel>(32 * 256, 16 * 1024 * 1024, buff.get());
         return;
     }
     if (host.test_feature(X86::Feature::avx2) && host.test_feature(X86::Feature::fma)) {
         std::cout << "AVX2:" << std::endl;
-        time_run<avx2::Kernel>(16 * 2 * 1024 * 1024, 1024, buff.get());
-        time_run<avx2::Kernel>(16 * 16 * 1024, 128 * 1024, buff.get());
-        time_run<avx2::Kernel>(16 * 2 * 1024, 1024 * 1024, buff.get());
-        time_run<avx2::Kernel>(16 * 128, 16 * 1024 * 1024, buff.get());
+        time_run<avx2::Kernel>(16 * 4 * 1024 * 1024, 1024, buff.get());
+        time_run<avx2::Kernel>(16 * 32 * 1024, 128 * 1024, buff.get());
+        time_run<avx2::Kernel>(16 * 4 * 1024, 1024 * 1024, buff.get());
+        time_run<avx2::Kernel>(16 * 256, 16 * 1024 * 1024, buff.get());
         return;
     }
     if (host.test_feature(X86::Feature::avx)) {
         std::cout << "AVX:" << std::endl;
-        time_run<avx::Kernel>(12 * 2 * 1024 * 1024, 1024, buff.get());
-        time_run<avx::Kernel>(12 * 16 * 1024, 128 * 1024, buff.get());
-        time_run<avx::Kernel>(12 * 2 * 1024, 1024 * 1024, buff.get());
-        time_run<avx::Kernel>(12 * 128, 16 * 1024 * 1024, buff.get());
+        time_run<avx::Kernel>(12 * 4 * 1024 * 1024, 1024, buff.get());
+        time_run<avx::Kernel>(12 * 32 * 1024, 128 * 1024, buff.get());
+        time_run<avx::Kernel>(12 * 4 * 1024, 1024 * 1024, buff.get());
+        time_run<avx::Kernel>(12 * 256, 16 * 1024 * 1024, buff.get());
         return;
     }
     std::cout << "SSE2:" << std::endl;
-    time_run<sse2::Kernel>(6 * 2 * 1024 * 1024, 1024, buff.get());
-    time_run<sse2::Kernel>(6 * 16 * 1024, 128 * 1024, buff.get());
-    time_run<sse2::Kernel>(6 * 2 * 1024, 1024 * 1024, buff.get());
-    time_run<sse2::Kernel>(6 * 128, 16 * 1024 * 1024, buff.get());
+    time_run<sse2::Kernel>(6 * 4 * 1024 * 1024, 1024, buff.get());
+    time_run<sse2::Kernel>(6 * 32 * 1024, 128 * 1024, buff.get());
+    time_run<sse2::Kernel>(6 * 4 * 1024, 1024 * 1024, buff.get());
+    time_run<sse2::Kernel>(6 * 256, 16 * 1024 * 1024, buff.get());
     return;
 #endif
 
 #if NACS_CPU_AARCH64
     std::cout << "ASIMD:" << std::endl;
-    time_run<asimd::Kernel>(6 * 2 * 1024 * 1024, 1024, buff.get());
-    time_run<asimd::Kernel>(6 * 16 * 1024, 128 * 1024, buff.get());
-    time_run<asimd::Kernel>(6 * 2 * 1024, 1024 * 1024, buff.get());
-    time_run<asimd::Kernel>(6 * 128, 16 * 1024 * 1024, buff.get());
+    time_run<asimd::Kernel>(6 * 4 * 1024 * 1024, 1024, buff.get());
+    time_run<asimd::Kernel>(6 * 32 * 1024, 128 * 1024, buff.get());
+    time_run<asimd::Kernel>(6 * 4 * 1024, 1024 * 1024, buff.get());
+    time_run<asimd::Kernel>(6 * 256, 16 * 1024 * 1024, buff.get());
     return;
 #endif
 
     std::cout << "Scalar:" << std::endl;
-    time_run<scalar::Kernel>(2 * 1024 * 1024, 1024, buff.get());
-    time_run<scalar::Kernel>(16 * 1024, 128 * 1024, buff.get());
-    time_run<scalar::Kernel>(2 * 1024, 1024 * 1024, buff.get());
-    time_run<scalar::Kernel>(128, 16 * 1024 * 1024, buff.get());
+    time_run<scalar::Kernel>(4 * 1024 * 1024, 1024, buff.get());
+    time_run<scalar::Kernel>(32 * 1024, 128 * 1024, buff.get());
+    time_run<scalar::Kernel>(4 * 1024, 1024 * 1024, buff.get());
+    time_run<scalar::Kernel>(256, 16 * 1024 * 1024, buff.get());
 }
 
 int main()
