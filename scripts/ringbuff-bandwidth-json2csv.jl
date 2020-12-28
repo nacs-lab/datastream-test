43bcbfeac6b90ff299b8161614b3dbf0bd5e455b
Yichao Yu <yyc1992@gmail.com>
2020-12-27 20:18:31 -0500

    [data] new test data including different block sizes

--- scripts/ringbuff-bandwidth-json2csv.jl
+++ scripts/ringbuff-bandwidth-json2csv.jl
@@ -7,21 +7,19 @@ lines = readlines(ARGS[1])
 all_res = []
 
 for i in 1:3:length(lines)
-    buff = parse.(Int, lines[i])
+    buff, blksz = parse.(Int, split(lines[i], '-'))
     datapipe = JSON.parse(lines[i + 1])
     blockring = JSON.parse(lines[i + 2])
-    push!(all_res, (size=buff, datapipe=datapipe, blockring=blockring))
-
-    "cycle" => 1.05632, "cache_ref" => 0.135487, "pipe_stall" => 2.22455e-7, "cache_miss" => 0.104959, "pipe_rw" => 2.38419e-7, "t" => 0.390561, "inst" => 0.500752, "pipe_sync" => 3.21039e-6
+    push!(all_res, (size=buff, blksz=blksz, datapipe=datapipe, blockring=blockring))
 end
 
-sort!(all_res, by=x->x.size)
+sort!(all_res, by=x->(x.size, x.blksz))
 
 function print_res(io, all_res, getter)
-    println(io, "Size (B),Time (ns),Inst,Cycle,Cache Ref,Cache Miss,Pipe RW,Pipe Stall,Pipe Sync")
+    println(io, "Size (B),Block Size (B),Time (ns),Inst,Cycle,Cache Ref,Cache Miss,Pipe RW,Pipe Stall,Pipe Sync")
     for res in all_res
         data = getter(res)
-        println(io, "$(res.size),$(data["t"]),$(data["inst"]),$(data["cycle"])," *
+        println(io, "$(res.size),$(res.blksz),$(data["t"]),$(data["inst"]),$(data["cycle"])," *
                 "$(data["cache_ref"]),$(data["cache_miss"])," *
                 "$(data["pipe_rw"]),$(data["pipe_stall"]),$(data["pipe_sync"])")
     end
