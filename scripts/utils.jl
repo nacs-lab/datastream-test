0c91ae29609d8c345f4eccb0e5ccf94ff7b64360
Yichao Yu <yyc1992@gmail.com>
2021-01-09 00:15:35 -0500

    [scripts] SI unit formatting function

--- scripts/utils.jl
+++ scripts/utils.jl
@@ -30,3 +30,46 @@ function size_to_str(size)
     end
     return "$(str(size)) P"
 end
+
+function num_to_si(num, unit)
+    str(num) = isinteger(num) ? string(Int(num)) : string(num)
+    if num >= 1e24
+        return "$(str(num / 1e24)) Y$unit"
+    elseif num >= 1e21
+        return "$(str(num / 1e21)) Z$unit"
+    elseif num >= 1e18
+        return "$(str(num / 1e18)) E$unit"
+    elseif num >= 1e15
+        return "$(str(num / 1e15)) P$unit"
+    elseif num >= 1e12
+        return "$(str(num / 1e12)) T$unit"
+    elseif num >= 1e9
+        return "$(str(num / 1e9)) G$unit"
+    elseif num >= 1e6
+        return "$(str(num / 1e6)) M$unit"
+    elseif num >= 1e3
+        return "$(str(num / 1e3)) k$unit"
+    elseif num >= 1
+        if isempty(unit)
+            return str(num)
+        else
+            return "$(str(num)) $unit"
+        end
+    elseif num >= 1e-3
+        return "$(str(num * 1e3)) m$unit"
+    elseif num >= 1e-6
+        return "$(str(num * 1e6)) μ$unit"
+    elseif num >= 1e-9
+        return "$(str(num * 1e9)) n$unit"
+    elseif num >= 1e-12
+        return "$(str(num * 1e12)) p$unit"
+    elseif num >= 1e-15
+        return "$(str(num * 1e15)) f$unit"
+    elseif num >= 1e-18
+        return "$(str(num * 1e18)) a$unit"
+    elseif num >= 1e-21
+        return "$(str(num * 1e21)) z$unit"
+    else # if num >= 1e-24
+        return "$(str(num * 1e24)) y$unit"
+    end
+end
